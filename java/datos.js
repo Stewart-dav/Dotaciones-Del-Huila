df35dd61dbd35bdbdd3fb23f2f6c12178da4b6f0
Stewart <stedalmur@gmail.com>
2026-09-15 10:26:23 -0500

    Add files via upload

---
+++ java/datos.js
@@ -0,0 +1,9 @@
+const inventario = [
+    { id: 1, categoria: "calzado", nombre: "Bota Robuster TROOPER Negra", precio: "$145.000" },
+    { id: 2, categoria: "calzado", nombre: "Bota Robuster TROOPER Blanca", precio: "$120.000" }, 
+    { id: 3, categoria: "calzado", nombre: "Bota Robuster TROOPER Café", precio: "$65.000" },
+    { id: 4, categoria: "impermeables", nombre: "Traje Impermeable PVC Dos Piezas", precio: "$85.000" },
+    { id: 5, categoria: "impermeables", nombre: "Capote Impermeable Reflectivo", precio: "$45.000" },
+    { id: 6, categoria: "protección visual", nombre: "Gafas de Seguridad Anti-empañante", precio: "$18.000" },
+    { id: 7, categoria: "guantes", nombre: "Guantes de Carnaza Reforzados", precio: "$15.000" }
+];
